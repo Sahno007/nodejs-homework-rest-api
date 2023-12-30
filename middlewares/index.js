694ad05e60709717623705e750d2cbb4ab2c36f6
Sahno007 <turnskin90@gmail.com>
2023-12-30 19:51:45 +0200

    emptyBody

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,5 @@
 const { validateContactBody } = require("./validateContactBody");
 const isValidId = require("./isValidId");
+const isEmptyReqBody = require("./isEmptyReqBody");
 
-module.exports = { validateContactBody, isValidId };
+module.exports = { validateContactBody, isValidId, isEmptyReqBody };
